f41b931323da4d027b049d8b26e33e58b4467f49
Onuigbo Clinton <32768102+Clinton594@users.noreply.github.com>
2022-04-18 16:59:31 +0100

    frontend ready

---
+++ constants/project.config.js
@@ -0,0 +1,13 @@
+const projectConfig = {
+  name: "CyberVerse",
+  favicon: "/favicon.ico",
+  logo: "/vercel.svg",
+  keywords: "ICO Presale Cryptocurrency",
+  description:
+    "Daowabunga utilizes Treasury Reserves to enable long-term price consistency and scarcity within an infinite supply system",
+  // Coins
+  token: "Coin Excel",
+  ticker: "CXL",
+};
+
+export default projectConfig;
